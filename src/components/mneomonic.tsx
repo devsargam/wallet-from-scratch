852c63670af9289aed9f14506803261a4688dd5b
Sargam Poudel <sargampoudel100@gmail.com>
2024-12-30 05:59:27 +0530

    feat: make mnemonic layout better

--- src/components/mneomonic.tsx
+++ src/components/mneomonic.tsx
@@ -4,9 +4,19 @@ export function Mnemonic({ mnemonic }: { mnemonic?: string }) {
   return (
     <section className="flex flex-col gap-y-10">
       <h2 className="text-2xl font-bold">Your Mnemonic</h2>
-      <div className="text-xl font-mono border border-white rounded-md p-4">
-        {mnemonic}
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+        {mnemonic.split(" ").map((word, index) => (
+          <MnemonicWord key={index} word={word} />
+        ))}
       </div>
     </section>
+  );
+}
+
+function MnemonicWord({ word }: { word: string }) {
+  return (
+    <div className="text-xl font-mono border border-white rounded-md p-4 hover:bg-gray-300 hover:text-black transition-all duration-500">
+      {word}
+    </div>
   );
 }
